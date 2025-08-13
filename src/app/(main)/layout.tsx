b8c2d9756578c4e373c54897dddc81cfe20233a0
koji-san <koji_777@nifty.com>
2025-08-13 22:37:29 +0900

    1st commit

---
+++ src/app/(main)/layout.tsx
@@ -0,0 +1,15 @@
+'use client':
+
+import React from 'react';
+// import SideMenu from '';
+
+const MainLayout={(children: React.reactNode)} =>{
+    return(
+        <div className='flex h-screen'>
+            {/* <SideMenu></SideMenu> */}
+            <main className='bg-slate-50 flex-1 overflow-auto'>{childlen}</main>
+        </div>
+    )
+}
+
+export default MainLayout;
---
+++ src/app/(main)/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+
+export default function Home() {
+  // throw new Error("This is an error");
+  return (
+    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
+      <p>main</p>
+    </div>
+  );
+}
